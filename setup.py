d2f503a14b31769deb3516102a23d77bc4c44e8f
albert pamonag <35522286+albertp16@users.noreply.github.com>
2024-05-09 14:22:06 +0800

    minor update aptools

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name = "aptool",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages(),
     install_requires = [
         #Add dependecies 
-    ]
+    ],
+    entry_points = {
+        "console_stripts" : [
+            "aptool = aptool:tools",
+        ]
+    }
 )
